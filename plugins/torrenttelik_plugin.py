10f8938e27c1b0c581a8376abd22125545a4b87a
andrey.malyshenko <andrey.malyshenko@gmail.com>
2017-09-30 15:27:24 +0200

    fixed allfon link, added debugging to torrent-telik but no luck so far

--- plugins/torrenttelik_plugin.py
+++ plugins/torrenttelik_plugin.py
@@ -25,6 +25,7 @@ class Torrenttelik(AceProxyPlugin):
     def downloadPlaylist(self, url):
         try:
             req = urllib2.Request(url, headers={'User-Agent' : "Magic Browser"})
+            Torrenttelik.logger.info("Getting list from: " + url)
             Torrenttelik.playlist = urllib2.urlopen(req, timeout=10).read()
             Torrenttelik.playlist = Torrenttelik.playlist.split('\xef\xbb\xbf')[1]     # garbage at the beginning
             Torrenttelik.playlist = Torrenttelik.playlist.replace(',\r\n]}', '\r\n]}') # excess comma at the end
